f514d1a540baf1041ce4bcb041222c81631dffc1
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-07-07 19:34:42 -0400

    Finished All tests in Models

--- src/main/java/models/DVDs.java
+++ src/main/java/models/DVDs.java
@@ -5,4 +5,51 @@ public class DVDs {
     private String name;
     private int qty;
     private float price;
+
+    public DVDs(Integer id, String name, Integer qty, Float price) {
+        this.id = id;
+        this.name = name;
+        this.qty = qty;
+        this.price = price;
+    }
+
+    public DVDs() {
+
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+
+
+    public void setQty(Integer qty){
+        this.qty = qty;
+    }
+
+    public Integer getQty(){
+        return this.qty;
+    }
+
+
+    public void setPrice(Float price){
+        this.price = price;
+    }
+
+    public Float getPrice() {
+        return this.price;
+    }
+
+
+
 }
--- src/test/java/models/DVDsTest.java
+++ src/test/java/models/DVDsTest.java
@@ -1,4 +1,75 @@
 package models;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DVDsTest {
+    @Test
+    public void setNameTest() {
+        // Given
+        String expected = "OZWEEGO";
+
+        // When
+        DVDs nameTest = new DVDs();
+        nameTest.setName(expected);
+
+        // Then
+        Assertions.assertEquals(expected, nameTest.getName());
+    }
+
+
+    @Test
+    public void setQtyTest() {
+        // Given
+        Integer qtyExpected = 50;
+        DVDs dvDs = new DVDs();
+        dvDs.setQty(qtyExpected);
+
+
+        // When
+        Integer qtyActual = dvDs.getQty();
+
+        // Then
+        Assertions.assertEquals(qtyExpected, qtyActual);
+    }
+
+
+    @Test
+    public void setPriceTest() {
+        // Given
+        Float priceExpected = 30F;
+        DVDs dvDs = new DVDs();
+        dvDs.setPrice(priceExpected);
+
+        // When
+        Float priceActual = dvDs.getPrice();
+
+
+        // Then
+        Assertions.assertEquals(priceExpected, priceActual, 0.01);
+
+
+
+
+    }
+
+    @Test
+    public void constructorTest() {
+        // Given
+
+        int expectedId = 6;
+        String expectedName = "Stan Smith";
+        Integer expectedQty = 10;
+        Float expectedPrice = 80.00f;
+
+        // When
+        DVDs testDVD = new DVDs(expectedId,expectedName, expectedQty, expectedPrice);
+
+
+        // Then
+        Assertions.assertEquals(expectedId, testDVD.getId());
+        Assertions.assertEquals(expectedName, testDVD.getName());
+        Assertions.assertEquals(expectedQty, testDVD.getQty());
+        Assertions.assertEquals(expectedPrice, testDVD.getPrice());
+    }
 }
