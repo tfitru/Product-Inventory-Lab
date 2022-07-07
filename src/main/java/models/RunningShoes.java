d50b15d6b8d8f7fee58585e8ae393341c40d2685
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-07-07 13:14:40 -0400

    added methods

--- src/main/java/models/RunningShoes.java
+++ src/main/java/models/RunningShoes.java
@@ -18,6 +18,41 @@ public class RunningShoes {
     public String getName() {
         return this.name;
     }
+
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getSize() {
+        return this.size;
+    }
+
+    public void setQty(Integer qty){
+        this.qty = qty;
+    }
+
+    public Integer getQty(){
+        return this.qty;
+    }
+
+
+    public void setPrice(Float price){
+        this.price = price;
+    }
+
+    public Float getPrice() {
+        return this.price;
+    }
+
+
 }
 
 
