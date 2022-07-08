cea6d00c3c384f359c22ab2043d00b65b347d2cd
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-07-07 22:01:12 -0400

    Got some work

--- src/main/java/services/RunningShoes.java
+++ src/main/java/services/RunningShoes.java
@@ -1,4 +1,35 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RunningShoes {
+    private static int nextId = 1; // (1)
+
+    private List<models.RunningShoes> inventory = new ArrayList<>();
+
+    public models.RunningShoes create(Integer id, String name, String brand, double size, Integer quantity, float price) {
+
+        models.RunningShoes createdShoes = new models.RunningShoes(nextId++, name, brand, size, quantity, price);
+
+        inventory.add(createdShoes);
+
+
+        return createdShoes;
+    }
+
+    // read
+    public RunningShoes findRunningShoes(int id) {
+        for(int i = 0; i<inventory.size(); i++){
+            if(inventory.get(i).equals(id)){
+                return inventory.get(i).getId();
+            }
+        }
+
+        return null;
+    }
+
+
+
+
 }
--- src/test/java/service/RunningShoesServiceTest.java
+++ src/test/java/service/RunningShoesServiceTest.java
@@ -1,4 +1,52 @@
 package service;
 
+
+import models.RunningShoesTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import services.RunningShoes;
+
 public class RunningShoesServiceTest {
+
+    @Test
+    public void createTest() {
+
+        // Given
+        Integer integerExpectedId = 5;
+        String expectedName = "Stan Smith";
+        String expectedBrand = "Adidas";
+        double expectedSize = 10.5;
+        Integer expectedQty = 10;
+        float expectedPrice = 50.00F;
+
+
+
+
+        // When
+        RunningShoes inventory = new RunningShoes();
+        models.RunningShoes inventoryTest = new models.RunningShoes(integerExpectedId, expectedName, expectedBrand, expectedSize, expectedQty, expectedPrice);
+
+
+        Integer actualId = inventoryTest.getId();
+        String actualName = inventoryTest.getName();
+        String actualBrand = inventoryTest.getBrand();
+        Double actualSize = inventoryTest.getSize();
+        Integer actualQty = inventoryTest.getQty();
+        float actualPrice = inventoryTest.getPrice();
+
+
+        // Then
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedBrand, actualBrand);
+        Assertions.assertEquals(expectedSize, actualSize);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+
+
+
+    }
+
+
+
 }
