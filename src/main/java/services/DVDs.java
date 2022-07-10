800a3a28d3a22b741f73d995c590eb95ffd5db79
Fitru <fitru@ZipcodersMBP100.fios-router.home>
2022-07-10 15:35:57 -0400

    ServiceDVD done

--- src/main/java/services/DVDs.java
+++ src/main/java/services/DVDs.java
@@ -1,4 +1,41 @@
 package services;
 
+import models.RunningShoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DVDs {
+    private static int nextId = 1; // (1)
+
+    models.DVDs run = new models.DVDs();
+
+    private List<models.DVDs> inventory = new ArrayList<>();
+
+    public models.DVDs create( String name, Integer qty, Float price) {
+        models.DVDs createdDVDs = new models.DVDs(nextId++, name, qty, price);
+        inventory.add(createdDVDs);
+
+        return createdDVDs;
+    }
+
+    // read
+
+    public models.DVDs findRunningShoes(int id){
+        return inventory.get(id);
+    }
+
+    public models.DVDs[] findAll() {
+        models.DVDs[] x = new models.DVDs[inventory.size()];
+        x = inventory.toArray(x);
+        return x;
+    }
+
+
+    public boolean delete(int id) {
+        if(inventory.contains(inventory.get(id))){
+            return true;
+        } else
+            return false;
+    }
 }
--- src/test/java/service/DVDsServiceTest.java
+++ src/test/java/service/DVDsServiceTest.java
@@ -1,4 +1,109 @@
 package service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import services.DVDs;
+import services.RunningShoes;
+
+import java.util.Arrays;
+
 public class DVDsServiceTest {
+    @Test
+    public void createTest() {
+
+        // Given
+        Integer integerExpectedId = 5;
+        String expectedName = "Stan Smith";
+        Integer expectedQty = 10;
+        float expectedPrice = 50.00F;
+
+
+
+        // When
+        DVDs inventory = new DVDs();
+        models.DVDs inventoryTest = new models.DVDs( integerExpectedId, expectedName, expectedQty, expectedPrice);
+
+
+        Integer actualId = inventoryTest.getId();
+        String actualName = inventoryTest.getName();
+        Integer actualQty = inventoryTest.getQty();
+        float actualPrice = inventoryTest.getPrice();
+
+
+        // Then
+        Assertions.assertEquals(Integer.class.getName(), new Integer(actualId).getClass().getName());
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedQty, actualQty);
+        Assertions.assertEquals(expectedPrice, actualPrice);
+
+
+
+    }
+
+
+    @Test
+    public void findRunningShoes(){
+        // Given
+        Integer integerExpectedId = 0;
+        String expectedName = "Stan Smith";
+        Integer expectedQty = 10;
+        float expectedPrice = 50.00F;
+        DVDs inventory = new DVDs();
+        models.DVDs inventoryTest = new models.DVDs( integerExpectedId, expectedName, expectedQty, expectedPrice);
+        inventory.create(expectedName, expectedQty, expectedPrice);
+
+
+        // When
+        inventory.findRunningShoes(0);
+        System.out.println(inventory.findRunningShoes(0));
+
+        // Then
+        Assertions.assertEquals(integerExpectedId, 0);
+
+    }
+
+
+    @Test
+    public void findAllTest() {
+        // Given
+        Integer integerExpectedId = 0;
+        String expectedName = "Stan Smith";
+        Integer expectedQty = 10;
+        float expectedPrice = 50.00F;
+        DVDs inventory = new DVDs();
+        models.DVDs inventoryTest = new models.DVDs( integerExpectedId, expectedName, expectedQty, expectedPrice);
+        inventory.create(expectedName, expectedQty, expectedPrice);
+
+        // When
+        inventory.findAll();
+
+
+
+
+        // Then
+        Assertions.assertEquals(Arrays.toString(inventory.findAll()), Arrays.toString(inventory.findAll()));
+        System.out.println(Arrays.toString(inventory.findAll()));
+
+    }
+
+    @Test
+    public void deleteTest() {
+        // Given
+        Integer integerExpectedId = 0;
+        String expectedName = "Stan Smith";
+        Integer expectedQty = 10;
+        float expectedPrice = 50.00F;
+        DVDs inventory = new DVDs();
+        models.DVDs inventoryTest = new models.DVDs( integerExpectedId, expectedName, expectedQty, expectedPrice);
+        inventory.create(expectedName, expectedQty, expectedPrice);
+
+
+        // When
+        inventory.delete(0);
+
+
+
+        // Then
+        Assertions.assertTrue(inventory.delete(0));
+    }
 }
